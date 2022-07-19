6030d694cf7bf0877a579d9554ea02acce5d1915
mjenifer <mike.jenifer@gmail.com>
2022-07-19 00:46:52 -0600

    Fic typo

--- runner.sh
+++ runner.sh
@@ -2,6 +2,6 @@
 
 i=0
 while ((++i <= ${NUMBER_OF_REQUESTS})); do
-  libcurl http://jps:5000/getjobdetails/$i >> /data/output.txt
+  curl http://jps:5000/getjobdetails/$i >> /data/output.txt
   echo >> /data/output.txt
 done
