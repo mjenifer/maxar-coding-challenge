9cd61aab590091dd91924cc75a4fed86c507d2a8
mjenifer <mike.jenifer@gmail.com>
2022-07-19 01:43:35 -0600

    Formatted output

--- runner.sh
+++ runner.sh
@@ -5,3 +5,11 @@ while ((++i <= ${NUMBER_OF_REQUESTS})); do
   curl http://jps:5000/getjobdetails/$i >> /data/output.txt
   echo >> /data/output.txt
 done
+
+echo "{\"jobs\": [" >> /data/formattedOutput.txt
+
+cat /data/output.txt | while read line
+do
+  grep -o '"jobId":"[^"]*' | grep -o '[^"]*$' | sed -e 's/^/"/g' -e 's/$/"/g' | tr '\n' ','>> /data/formattedOutput.txt
+done
+echo "]}" >> /data/formattedOutput.txt
